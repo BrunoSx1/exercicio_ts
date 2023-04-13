801873b781029157e1bdaf0db9ac99a3978d656a
BrunoSx1 <s.bruno.12@hotmail.com>
2023-04-13 08:36:33 -0300

    Corrigindo erro

--- src/exercicio.ts
+++ src/exercicio.ts
@@ -5,7 +5,7 @@ function saudacao(nome: string): string {
 function multiplicar(num1: number, num2: number): number {
     return num1 * num2;
 }
-console.log(saudacao("EBAC, o resultado é:")); // deve imprimir "Olá Fulano" no terminal
-console.log(multiplicar(150, 3)); // deve imprimir 6 no terminal
+console.log(saudacao("EBAC, o resultado é:")); 
+console.log(multiplicar(150, 3)); 
 
 
