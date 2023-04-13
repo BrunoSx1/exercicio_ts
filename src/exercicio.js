657af90010f9a7557426e229fb2d2c671b2e262e
BrunoSx1 <s.bruno.12@hotmail.com>
2023-04-13 05:52:13 -0300

    first commit

---
+++ src/exercicio.js
@@ -0,0 +1,9 @@
+"use strict";
+function saudacao(nome) {
+    return "Olá " + nome;
+}
+function multiplicar(num1, num2) {
+    return num1 * num2;
+}
+console.log(saudacao("EBAC, o resultado é:")); // deve imprimir "Olá Fulano" no terminal
+console.log(multiplicar(150, 3)); // deve imprimir 6 no terminal
---
+++ src/exercicio.ts
@@ -0,0 +1,11 @@
+function saudacao(nome: string): string {
+    return "Olá " + nome;
+}
+
+function multiplicar(num1: number, num2: number): number {
+    return num1 * num2;
+}
+console.log(saudacao("EBAC, o resultado é:")); // deve imprimir "Olá Fulano" no terminal
+console.log(multiplicar(150, 3)); // deve imprimir 6 no terminal
+
+
